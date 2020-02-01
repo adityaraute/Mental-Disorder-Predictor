06904f0af3c4d663f6d7b1ca160e88b39d029749
rj8228 <rj8228@gmail.com>
2020-02-01 18:38:03 +0530

    firebase integration done

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,18 +1,19 @@
-import * as firebase from 'firebase';
-import firestore from 'firebase/firestore'
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-const settings = {timestampsInSnapshots: true};
 
 const config = {
   apiKey: "AIzaSyBBKhFJuohizEVmMu5T93GJn4el9NovXdw",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
+  authDomain: "te-project-e0020.firebaseapp.com",
+  databaseURL: "https://te-project-e0020.firebaseio.com",
   projectId: "te-project-e0020",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_ID"
+  storageBucket: "te-project-e0020.appspot.com",
+  messagingSenderId: "515648217487",
+  appId: "1:515648217487:web:1e46322c5d015b9e72ebfa",
+  measurementId: "G-G3H4VCQK6Y"
 };
+
 firebase.initializeApp(config);
 
-firebase.firestore().settings(settings);
 
 export default firebase;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,20 +1,46 @@
 import React , {Component}from 'react';
 import classes from './Home.module.css';
+import firebase from '../../firebase';
 class Home extends Component{
+
+    constructor(props){
+        super(props);
+        this.formSub = this.formSub.bind(this);
+        this.db = firebase.firestore();
+        console.log(this.db);
+        
+
+    }
+
+    formSub(){
+        console.log("submitted");
+        //this is the error ! 
+        //#lols     
+        let docRef = this.db.collection('users').doc('alovelace');
+        console.log(docRef);
+        docRef.set({
+        first: 'Ada',
+        last: 'Lovelace',
+        born: 1815
+        });
+   
+    }
+
+    componentDidMount(){
+
+    }
     render(){
         return(
             <div className={classes.container}>
-                <form>
-                    <label for="username">Username</label>
+                <form onSubmit={this.formSub}>
+                    <label htmlFor="username">Username</label>
                     <input type="text" id="username" placeholder="Username" />
-                    <label for="passwd">Password</label>
+                    <label htmlFor="passwd">Password</label>
                     <input type="password" id="passwd" placeholder="Password" />
-
+                    <input type="submit" title="submit"></input>
                 </form>
             </div>
         );
-
-        
     }
 }
 export default Home;
