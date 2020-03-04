9b8e62e2fc0c6229bae12149c16753ae8f252955
rj8228 <rj8228@gmail.com>
2020-03-05 01:28:26 +0530

    autism form and api done

--- src/components/UserComponents/Autism/Autism.js
+++ src/components/UserComponents/Autism/Autism.js
@@ -42,30 +42,12 @@ class Autism extends Component{
             .then(function (response) {
                 //handle success
                 console.log(response);
+                console.log("autism prediction",response.data.results.prediction);
             })
             .catch(function (response) {
                 //handle error
                 console.log(response);
             });
-
-        // axios.post('https://gentle-caverns-95040.herokuapp.com/',JSON.stringify({
-                    
-        //             'q2':0,
-        //             'q3':1,
-        //             'q4':0,
-        //             'q5':1,
-        //             'q6':0,
-        //             'q7':1,
-        //             'q8':0,
-        //             'q9':1,
-        //             'q10':0
-        //     	})).then( response => {
-        //             console.log(response);
-        //         }).catch( error => {
-        //             console.log(error);
-        //         })
-
-       
 
     }
     componentDidMount() {
@@ -75,113 +57,124 @@ class Autism extends Component{
     render(){
         return(
         <form onSubmit={this.formSub}>
-                    <h5>Autism questions</h5>
+                    
+                    
+<h5>Autism questions</h5>
 
                     
-<p>Does your child look at you when you call his/her name?
+<p>
+    1)Does your child look at you when you call his/her name?
 </p>
-<input type="radio" name="" value="always"> always
-<input type="radio" name="" value="usually">usually
-<input type="radio" name="" value="usually">sometimes
-<input type="radio" name="" value="usually">rarely
-<input type="radio" name="" value="usually">never
 
-<p>How easy is it for you to get eye contact with your child? 
+<input type="radio" name="always" value="always"/>always
+<input type="radio" name="usually" value="usually"/>usually
+<input type="radio" name="sometimes" value="sometimes"/>sometimes
+<input type="radio" name="rarely" value="rarely"/>rarely
+<input type="radio" name="never" value="usually"/>usually
+<p>
+    2)How easy is it for you to get eye contact with your child? 
 </p>
-<input type="radio" name="" value="always"> always
-<input type="radio" name="" value="usually">usually
-<input type="radio" name="" value="usually">sometimes
-<input type="radio" name="" value="usually">rarely
-<input type="radio" name="" value="usually">never
 
-<p>Does your child point to indicate that s/he wants something? (e.g. a toy that is 
+<input type="radio" name="always" value="always"/>always
+<input type="radio" name="usually" value="usually"/>usually
+<input type="radio" name="sometimes" value="sometimes"/>sometimes
+<input type="radio" name="rarely" value="rarely"/>rarely
+<input type="radio" name="never" value="usually"/>usually
+
+<p>
+    3)Does your child point to indicate that s/he wants something? (e.g. a toy that is 
 out of reach) 
 </p>
-<input type="radio" name="" value="always"> always
-<input type="radio" name="" value="usually">usually
-<input type="radio" name="" value="usually">sometimes
-<input type="radio" name="" value="usually">rarely
-<input type="radio" name="" value="usually">never
 
+<input type="radio" name="always" value="always"/>always
+<input type="radio" name="usually" value="usually"/>usually
+<input type="radio" name="sometimes" value="sometimes"/>sometimes
+<input type="radio" name="rarely" value="rarely"/>rarely
+<input type="radio" name="never" value="usually"/>usually
 <p>
-Does your child point to share interest with you? (e.g. poin9ng at an 
-interes9ng sight) 
+4)Does your child point to share interest with you? (e.g. pointing at an 
+interesting sight) 
 </p>
-<input type="radio" name="" value="always"> always
-<input type="radio" name="" value="usually">usually
-<input type="radio" name="" value="usually">sometimes
-<input type="radio" name="" value="usually">rarely
-<input type="radio" name="" value="usually">never
 
+<input type="radio" name="always" value="always"/>always
+<input type="radio" name="usually" value="usually"/>usually
+<input type="radio" name="sometimes" value="sometimes"/>sometimes
+<input type="radio" name="rarely" value="rarely"/>rarely
+<input type="radio" name="never" value="usually"/>usually
 <p>
-Does your child pretend? (e.g. care for dolls, talk on a toy phone) 
+5)Does your child pretend? (e.g. care for dolls, talk on a toy phone) 
 </p>
-<input type="radio" name="" value="always"> always
-<input type="radio" name="" value="usually">usually
-<input type="radio" name="" value="usually">sometimes
-<input type="radio" name="" value="usually">rarely
-<input type="radio" name="" value="usually">never
+
+<input type="radio" name="always" value="always"/>always
+<input type="radio" name="usually" value="usually"/>usually
+<input type="radio" name="sometimes" value="sometimes"/>sometimes
+<input type="radio" name="rarely" value="rarely"/>rarely
+<input type="radio" name="never" value="usually"/>usually
 
 <p>
-Does your child follow where you’re looking? 
+6)Does your child follow where you’re looking? 
 </p>
-<input type="radio" name="" value="always"> always
-<input type="radio" name="" value="usually">usually
-<input type="radio" name="" value="usually">sometimes
-<input type="radio" name="" value="usually">rarely
-<input type="radio" name="" value="usually">never
+
+<input type="radio" name="always" value="always"/>always
+<input type="radio" name="usually" value="usually"/>usually
+<input type="radio" name="sometimes" value="sometimes"/>sometimes
+<input type="radio" name="rarely" value="rarely"/>rarely
+<input type="radio" name="never" value="usually"/>usually
 
 <p>
-If you or someone else in the family is visibly upset, does your child show signs 
-of wan9ng to comfort them? (e.g. stroking hair, hugging them)
+7)If you or someone else in the family is visibly upset, does your child show signs 
+of wanting to comfort them? (e.g. stroking hair, hugging them)
 </p>
-<input type="radio" name="" value="always"> always
-<input type="radio" name="" value="usually">usually
-<input type="radio" name="" value="usually">sometimes
-<input type="radio" name="" value="usually">rarely
-<input type="radio" name="" value="usually">never
+
+<input type="radio" name="always" value="always"/>always
+<input type="radio" name="usually" value="usually"/>usually
+<input type="radio" name="sometimes" value="sometimes"/>sometimes
+<input type="radio" name="rarely" value="rarely"/>rarely
+<input type="radio" name="never" value="usually"/>usually
 
 <p>
-Would you describe your child’s first words as:
+8)Would you describe your child’s first words as:
 </p>
-<input type="radio" name="" value="always"> always
-<input type="radio" name="" value="usually">usually
-<input type="radio" name="" value="usually">sometimes
-<input type="radio" name="" value="usually">rarely
-<input type="radio" name="" value="usually">never
+
+<input type="radio" name="always" value="always"/>always
+<input type="radio" name="usually" value="usually"/>usually
+<input type="radio" name="sometimes" value="sometimes"/>sometimes
+<input type="radio" name="rarely" value="rarely"/>rarely
+<input type="radio" name="never" value="usually"/>usually
+
 
 <p>
-Does your child use simple gestures? (e.g. wave goodbye) 
+9)Does your child use simple gestures? (e.g. wave goodbye) 
 </p>
-<input type="radio" name="" value="always"> always
-<input type="radio" name="" value="usually">usually
-<input type="radio" name="" value="usually">sometimes
-<input type="radio" name="" value="usually">rarely
-<input type="radio" name="" value="usually">never
+
+<input type="radio" name="always" value="always"/>always
+<input type="radio" name="usually" value="usually"/>usually
+<input type="radio" name="sometimes" value="sometimes"/>sometimes
+<input type="radio" name="rarely" value="rarely"/>rarely
+<input type="radio" name="never" value="usually"/>usually
 
 <p>
 
-Does your child stare at nothing with no apparent purpose? 
+10)Does your child stare at nothing with no apparent purpose? 
  
 </p>
-<input type="radio" name="" value="always"> always
-<input type="radio" name="" value="usually">usually
-<input type="radio" name="" value="usually">sometimes
-<input type="radio" name="" value="usually">rarely
-<input type="radio" name="" value="usually">never
 
+<input type="radio" name="always" value="always"/>always
+<input type="radio" name="usually" value="usually"/>usually
+<input type="radio" name="sometimes" value="sometimes"/>sometimes
+<input type="radio" name="rarely" value="rarely"/>rarely
+<input type="radio" name="never" value="usually"/>usually
 
+<br></br>
+<br></br>
+<br></br>
 
+<input type="submit" title="submit"></input>
+<br></br>
+<br></br>
+<br></br>
 
-
-
-
-
-
-
-
-
-                </form>
+</form>
         )
     }
     
