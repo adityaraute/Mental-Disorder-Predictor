8b63988a9ceb3dd362bb3e8c1a690dae43e2e96c
rj8228 <rj8228@gmail.com>
2020-03-07 16:39:11 +0530

    Revolution!!!!!!!

--- src/components/UserComponents/CB/CB.js
+++ src/components/UserComponents/CB/CB.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Auxiliary from '../../../containers/Auxiliary/Auxiliary';
+import { black } from 'material-ui/styles/colors';
 
 class CB extends Component{
     constructor(props) {
@@ -27,19 +29,24 @@ class CB extends Component{
     }
     render(){
         return(
-        <form onSubmit={this.formSub}>
-                    <h5>CB questions</h5>
+            <Auxiliary>
+            <form onSubmit={this.formSub}>
+                        <h5>CB questions</h5>
 
-                    <label htmlFor="one">Q1</label>
-                    <input type="text" id="one" placeholder="One" onChange={this.changehandler} value={this.state.one} /><br></br>
-                    <label htmlFor="two">Q2</label>
-                    <input type="text" id="two" placeholder="Two" onChange={this.changehandler} value={this.state.two} /><br></br>
-                    <label htmlFor="three">Q3</label>
-                    <input type="text" id="three" placeholder="Three" onChange={this.changehandler} value={this.state.three} /><br></br>
-                    <label htmlFor="four">Q4</label>
-                    <input type="text" id="four" placeholder="Four" onChange={this.changehandler} value={this.state.four} />
-                    <input type="submit" title="submit"></input>
-                </form>
+                        <label htmlFor="one">Q1</label>
+                        <input type="text" id="one" placeholder="One" onChange={this.changehandler} value={this.state.one} /><br></br>
+                        <label htmlFor="two">Q2</label>
+                        <input type="text" id="two" placeholder="Two" onChange={this.changehandler} value={this.state.two} /><br></br>
+                        <label htmlFor="three">Q3</label>
+                        <input type="text" id="three" placeh    older="Three" onChange={this.changehandler} value={this.state.three} /><br></br>
+                        <label htmlFor="four">Q4</label>
+                        <input type="text" id="four" placeholder="Four" onChange={this.changehandler} value={this.state.four} />
+                        <input type="submit" title="submit"></input>
+                    </form>
+            <div style={{backgroundColor:"#000"}}>
+                <img src={ require("../../../Assets/plates/ishihara.svg")} alt="rect"></img>
+            </div>
+        </Auxiliary>
         )
     }
     
