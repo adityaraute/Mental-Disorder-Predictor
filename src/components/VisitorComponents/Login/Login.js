5b82b4d5ab47ff1ce5543fdb5854cccb6ff458de
rj8228 <rj8228@gmail.com>
2020-03-30 20:14:24 +0530

    react-create-app on Heroku

--- src/components/VisitorComponents/Login/Login.js
+++ src/components/VisitorComponents/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import firebase from '../../../firebase';
+import firebase from './firebase';
 import { Redirect } from 'react-router-dom';
 import TextField from '@material-ui/core/TextField';
 import Container from '@material-ui/core/Container';
